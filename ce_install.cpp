f35ff61fb593a92665636aa6c29d4082481a550b
CarlosEspadinha <48103291+CarlosEspadinha@users.noreply.github.com>
2022-04-13 12:59:56 +0100

    Add files via upload

--- ce_install.cpp
+++ ce_install.cpp
@@ -4,26 +4,132 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <string>
-#include "ce_bash.h"
+#include <map>
+#include <vector>
+#include <fstream>
 using namespace std;
 
-//CONFIG INSTALL
-const char* CE_ALL = "/CE_ALL";
-const char* APP_NAME = "Simul4";
-const char* APP_FOLDER = "/PUBLIC-Simul4";
-const char* GIT_WGET = "https://github.com/CarlosEspadinha/PUBLIC/archive/refs/heads/Simul4.zip";
-const char* EXEC = "simul";
+class ce_bash {
+	public:
+		ce_bash(string file) {
+			_file = file;
+			ifstream bash(file);
+			string line;
+			string status = "before";
+			while (getline(bash, line)) {
+				if (line == "###__CE_EPPS_BASH__") {
+					status = "after";
+				}
+				if (status == "before") {
+					before.push_back(line);
+				} else if (status == "self") {
+					self.push_back(line);
+				} else {
+					after.push_back(line);
+				}
+				if (line == "###__CE_APPS_BASH__") {
+					status = "self";
+				}
+			}
+			this->convert_map();
+			bash.close();
+		}
+		void convert_map() {
+			if (self.size()%3 !=0) throw 0;
+			for (int i=0; i<self.size(); i+=3) {
+				dict[self[i]] = self[i+1];
+			}
+		}
+		void convert_vec() {
+			self.clear();
+			map<string,string>::iterator it;
+			for(it=dict.begin(); it!=dict.end(); ++it) {
+				self.push_back(it->first);
+				self.push_back(it->second);
+				self.push_back("#");
+			}
+		}
+		void add(string key, string obj) {
+			dict[key] = obj;
+		}
+		void addfile(string file) {
+			ifstream bashf(file);
+			string line;
+			vector<string> vec;
+			while (getline(bashf, line)) {
+				vec.push_back(line);
+			}
+			if (vec.size()%3 == 0) {
+				for (int i=0; i<vec.size(); i+=3) {
+					dict[vec[i]] = vec[i+1];
+				}
+			}
+			bashf.close();
+		}
+		void del(string key) {
+			dict.erase(key);
+		}
+		void delfile(string file) {
+			ifstream bashf(file);
+			string line;
+			vector<string> vec;
+			while (getline(bashf, line)) {
+				vec.push_back(line);
+			}
+			if (vec.size()%3 == 0) {
+				for (int i=0; i<vec.size(); i+=3) {
+					dict.erase(vec[i]);
+				}
+			}
+			bashf.close();
+		}
+		void write_out() {
+			ofstream bash(_file);
+			for (int i=0; i<before.size(); i++) {
+				bash << before[i] << endl;
+			}
+			for (int i=0; i<self.size(); i++) {
+				bash << self[i] << endl;
+			}
+			for (int i=0; i<after.size(); i++) {
+				bash << after[i] << endl;
+			}
+			bash.close();
+		}
+		void install() {
+			if (after.size() == 0) {
+				before.push_back("###__CE_APPS_BASH__");
+				after.push_back("###__CE_EPPS_BASH__");
+			}
+			this->convert_vec();
+			this->write_out();
+		}
+		void uninstall() {
+			if (after.size() != 0) {
+				before.pop_back();
+				after.erase(after.begin());
+				self.clear();
+				this->write_out();
+			}
+		}
+	private:
+		string _file;
+		vector<string> before;
+		vector<string> self;
+		vector<string> after;
+		map<string,string> dict;
+};
 
 //Global
 string path, message;
 
 void INSTALL(ce_bash& bash, string program) {
-	char* APP_FOLDER, GIT_WGET, EXEC;
+	string APP_FOLDER, GIT_WGET, EXEC;
 	bool p = true;
 	if (program == "self" || program == "update") {
 		cout << "\"Updating\" ce_install" << endl;
 		p = false;
-
+		GIT_WGET = "https://raw.githubusercontent.com/CarlosEspadinha/PUBLIC/main/ce_install.cpp";
 		message = "wget -q -O " + path + "/last.zip " + GIT_WGET;
 		system(message.c_str());
 		message = "g++ -std=c++11 " + path + "/ce_install.cpp -o " + path + "/ce_install";
@@ -75,7 +181,7 @@ void INSTALL(ce_bash& bash, string program) {
 int main(int argc, char *argv[]) {
 	path = getpwuid(getuid())->pw_dir;
 	ce_bash bash(path + "/.bashrc");
-	path += CE_ALL;
+	path += "/CE_ALL";
 	mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 	if (argc < 2) INSTALL(bash, "self");
 	else {
